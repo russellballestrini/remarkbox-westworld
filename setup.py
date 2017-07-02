977aa2ffa0bb453b838d23e8cfdd8d851f5af400
russellballestrini <russell.ballestrini@gmail.com>
2017-07-01 21:02:20 -0400

    Remarkbox has a new theme system!
    	renamed:    remarkbox_westworld/static/theme/css/westworld.css -> remarkbox_westworld/static/theme/westworld/css/theme.css
    	renamed:    remarkbox_westworld/static/theme/img/westworld2-bg.jpg -> remarkbox_westworld/static/theme/westworld/img/westworld2-bg.jpg
    	renamed:    remarkbox_westworld/templates/base.j2 -> remarkbox_westworld/templates/westworld-base.j2
    	modified:   setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
-import os
+# A Remarkbox theme is a python package with the following paths:
+# 
+#   template/<theme_name>-base.j2
+#   static/theme/<theme_name>
+#
+# The theme needs to be registered to the 'remarkbox.themes'
+# entry_point group to make it loaded by the Remarkbox application.
 
 from setuptools import setup, find_packages
 
 setup(name='remarkbox_westworld',
-      version='0.0.1',
+      version='0.2.0',
       description='remarkbox westworld theme',
       classifiers=[
         "Programming Language :: Python",
@@ -14,9 +20,14 @@ setup(name='remarkbox_westworld',
       author='Russell Ballestrini',
       author_email='russell@ballestrini.net',
       url='http://russell.ballestrini.net',
-      keywords='remarkbox theme',
+      keywords='remarkbox westworld theme',
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
+      entry_points = {
+        'remarkbox.themes' : [
+            'westworld = remarkbox_westworld'
+        ],
+      },
 )
 
